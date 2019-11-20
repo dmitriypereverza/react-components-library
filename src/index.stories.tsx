2be56034032e85fd5754713f3bceb265873dee26
Дмитрий Переверза <d.pereverza@worksolutions.ru>
2019-11-20 19:58:50 +0300

    init

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs/react';
 
 import Typography from 'primitives/Typography';
+import ButtonComponent, { ButtonType } from "primitives/Button";
 
 const stories = storiesOf('Components', module);
+stories.addDecorator(withKnobs);
+stories.add(
+  'Typography',
+  () => <Typography type={"regularBody1"}>{text("Text", "Тестовый текст")}</Typography>,
+  { info: { text: 'sdfsdf sd fsdf' } }
+);
 
 stories.add(
   'Button',
-  () => <Typography type={"regularBody1"}>Hello</Typography>,
+  () => <ButtonComponent type={ButtonType.primary} onClick={action("onClick")}>Hello</ButtonComponent>,
   { info: { inline: true } }
 );
